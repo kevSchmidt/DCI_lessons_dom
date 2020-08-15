5a37476ec9ce515c271f3a70d60b27dd48f0fac9
Kevin <kev.saintigny.schmidt@gmail.com>
2020-08-15 15:42:33 +0200

    dom

---
+++ README.md
@@ -0,0 +1,6 @@
+# Document Object Model
+
+- Lessons
+- Exercises
+
+![](./dom.png)
---
+++ lessons/weather/src/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const weather = require("./lib/weather");
+const args = process.argv.slice(2);
+// npm start Berlin Germany
+weather(args[0], args[1]).then(console.log).catch(console.error);
